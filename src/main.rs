60db440d476c4e8aee5c5b266a0d26c776f8a686
Shinya Tsuda <shinya@dacci.org>
2022-11-12 08:00:23 +0900

    Upgrade to clap v4.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use anyhow::Result;
 use clap::Parser;
 
 #[derive(Debug, clap::Parser)]
-#[clap(about, version)]
+#[command(about, version)]
 enum Args {
-    #[clap(subcommand)]
+    #[command(subcommand)]
     Json(json::Args),
 
-    #[clap(subcommand)]
+    #[command(subcommand)]
     Xml(xml::Args),
 }
 
